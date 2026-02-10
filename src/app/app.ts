13586f82f70c4a6512f7a51ade21daa84f892236
Guillaume G <guillaume.gerard2112@outlook.fr>
2026-02-10 10:30:45 +0100

    feat : ajout d'un premier composant note

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Note } from './models/note';
+import { NoteComponent } from './note/note.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NoteComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('note');
+
+  myNote!: Note;
+
+  ngOnInit() {
+    this.myNote = new Note(1, 'Note 1', 'This is the content of note 1');
+  }
 }
